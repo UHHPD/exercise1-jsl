a40ffdf28edc547ef4640580aa88a132bc72d53d
jomatthi <82223346+jomatthi@users.noreply.github.com>
2021-10-20 13:17:45 +0000

    Aufgabe 2

--- summieren.cc
+++ summieren.cc
@@ -2,17 +2,17 @@
 #include <fstream>
 
 int main() {
-  std::ifstream fin("daten.txt");
-  double summand_1;
-  double summand_2;
+  //Define Input textfile
+  std::ifstream fin("daten.txt"); 
+  double summand_1; //first summand
+  double summand_2; //second summand 
+  //Define Output textfile
+  std::ofstream fout("datensumme.txt");
   while (fin >> summand_1 >> summand_2){
     double summe;
-    summe = summand_1 + summand_2;
-    std::ofstream fout("summe_daten.txt");
-    fout << summe << std::endl;
-    fout.close();
-    std::cout << summe << std::endl;
+    summe = summand_1 + summand_2; //Sum the two values
+    fout << summe << std::endl; //Write into Output textfile
   }
-  //std::cout << summand_1 << summand_2 << summe << std::endl;
-  fin.close();
+  fout.close(); //Close Output textfile
+  fin.close(); //Close Input textfile
 }
